1b01c83472abcc2bbb42f7f8eeed450f2da3ab2d
chenlijun <chenlijun@chenlijun.local>
2021-04-02 19:57:23 +0800

    0.3.0-alpha3

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import {loadScript} from './utils'
 const Sk = window.Sk;
 const libList = {
-  "./pgzrun/__init__.js": "./dist/pygame-zero.js",
+  // "./pgzrun/__init__.js": "./dist/pygame-zero.js",
   // "./pgzrun/__init__.js": "https://cdn.jsdelivr.net/gh/lipten/skulpt-pygame-zero/dist/pygame-zero.js",
-  // "./pgzrun/__init__.js": "https://cdn.jsdelivr.net/gh/lipten/skulpt-pygame-zero@physics-engine/dist/pygame-zero.js",
+  "./pgzrun/__init__.js": "https://cdn.jsdelivr.net/gh/lipten/skulpt-pygame-zero@physics-engine/dist/pygame-zero.js",
 }
 export const PyGameZero = {
   usePyGameZero: function(readFn) {
